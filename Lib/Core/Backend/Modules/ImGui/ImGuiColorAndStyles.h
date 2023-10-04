6d8817cf55fc76e58ae7faad0e7d158c923b8bca
Joao <jstreibel@gmail.com>
2023-10-03 23:00:28 -0300

    Organized some ImGui styles.

---
+++ Lib/Core/Backend/Modules/ImGui/ImGuiColorAndStyles.h
@@ -0,0 +1,21 @@
+//
+// Created by joao on 3/10/23.
+//
+
+#ifndef STUDIOSLAB_IMGUICOLORANDSTYLES_H
+#define STUDIOSLAB_IMGUICOLORANDSTYLES_H
+
+#include "3rdParty/imgui/imgui.h"
+
+namespace Core {
+
+    void SetColorThemeNativeLight();
+    void SetColorThemeNativeDark();
+    void SetColorThemeDarkRed();
+    void SetStyleDark();
+    void SetStyleLight();
+    void SetStyleStudioSlab();
+
+}
+
+#endif //STUDIOSLAB_IMGUICOLORANDSTYLES_H
