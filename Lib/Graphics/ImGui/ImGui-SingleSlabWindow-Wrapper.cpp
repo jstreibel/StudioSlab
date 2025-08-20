020bb6a6e147f35927b05dc4fa78c752df3b0d81
joao <jstreibel@gmail.com>
2025-08-19 23:09:43 -0300

    Passing events to SlabWindow under ImGuiWindow

--- Lib/Graphics/ImGui/ImGui-SingleSlabWindow-Wrapper.cpp
+++ Lib/Graphics/ImGui/ImGui-SingleSlabWindow-Wrapper.cpp
@@ -71,85 +71,38 @@ namespace Slab::Graphics {
             if(Fix WindowFlags = ImGuiWindowFlags_NoCollapse;
                ImGui::Begin(UniqueId.c_str(), nullptr, WindowFlags))
             {
+                fix Pos = ImGui::GetWindowPos();
+                fix ContentMin = ImGui::GetWindowContentRegionMin();
+                fix ContentMax = ImGui::GetWindowContentRegionMax();
 
-                if constexpr (false)
+                fix w = ContentMax.x - ContentMin.x;
+                fix h = ContentMax.y - ContentMin.y;
+                fix x = Pos.x + ContentMin.x;
+                fix y = Pos.y + ContentMin.y;
+                // constexpr auto Min = 400;
+                // fix w = dim.x > Min ? dim.x : Min;
+                // fix h = dim.y > Min ? dim.y : Min;
+
+                SlabWindow->Set_x(static_cast<int>(x));
+                // slab_window->sety(backend.getScreenHeight() - (y + dim.y));
+                SlabWindow->Set_y(static_cast<int>(y));
+                SlabWindow->NotifyReshape(static_cast<int>(w), static_cast<int>(h));
+
+                // Further defer
+                auto Callback = [](const ImDrawList *ParentList, const ImDrawCmd *DrawCommand)
                 {
+                    OpenGL::FGLStateGuard StateGuard{};
 
-                }
-                else if constexpr (false)
-                {
-                    // Outer table: 2 columns
-                    if (ImGui::BeginTable("OuterTable", 2,
-                            ImGuiTableFlags_BordersInner | ImGuiTableFlags_Resizable))
-                    {
-                        // Setup widths
-                        ImGui::TableSetupColumn("Left",  ImGuiTableColumnFlags_WidthFixed,  200);
-                        ImGui::TableSetupColumn("Right", ImGuiTableColumnFlags_WidthStretch);
+                    if (DrawCommand->UserCallback == nullptr) return;
 
-                        // First row
-                        ImGui::TableNextRow();
-                        ImGui::TableSetColumnIndex(0);
-                        ImGui::Text("Outer C0,R0");
+                    const auto& [SlabWindow, PlatformWindow] = *static_cast<FCallbackData*>(DrawCommand->UserCallbackData);
 
-                        ImGui::TableSetColumnIndex(1);
-                        ImGui::Text("Outer C1,R0");
+                    glPushAttrib(GL_VIEWPORT_BIT);
+                    SlabWindow->ImmediateDraw(*PlatformWindow);
+                    glPopAttrib();
+                };
 
-                        // Second row: nest an inner table in C0
-                        ImGui::TableNextRow();
-                        ImGui::TableSetColumnIndex(0);
-                        if (ImGui::BeginTable("InnerTable", 2, ImGuiTableFlags_Borders))
-                        {
-                            ImGui::TableNextRow();
-                            ImGui::TableSetColumnIndex(0); ImGui::Text("I C0,R0");
-                            ImGui::TableSetColumnIndex(1); ImGui::Text("I C1,R0");
-                            ImGui::TableNextRow();
-                            ImGui::TableSetColumnIndex(0); ImGui::Text("I C0,R1");
-                            ImGui::TableSetColumnIndex(1); ImGui::Text("I C1,R1");
-                            ImGui::EndTable();
-                        }
-
-                        // You can also put any other widgets in the Right cell:
-                        ImGui::TableSetColumnIndex(1);
-                        ImGui::Button("Click me");
-
-                        ImGui::EndTable();
-                    }
-                }
-                else
-                {
-                    fix Pos = ImGui::GetWindowPos();
-                    fix ContentMin = ImGui::GetWindowContentRegionMin();
-                    fix ContentMax = ImGui::GetWindowContentRegionMax();
-
-                    fix w = ContentMax.x - ContentMin.x;
-                    fix h = ContentMax.y - ContentMin.y;
-                    fix x = Pos.x + ContentMin.x;
-                    fix y = Pos.y + ContentMin.y;
-                    // constexpr auto Min = 400;
-                    // fix w = dim.x > Min ? dim.x : Min;
-                    // fix h = dim.y > Min ? dim.y : Min;
-
-                    SlabWindow->Set_x(static_cast<int>(x));
-                    // slab_window->sety(backend.getScreenHeight() - (y + dim.y));
-                    SlabWindow->Set_y(static_cast<int>(y));
-                    SlabWindow->NotifyReshape(static_cast<int>(w), static_cast<int>(h));
-
-                    // Further defer
-                    auto Callback = [](const ImDrawList *ParentList, const ImDrawCmd *DrawCommand)
-                    {
-                        OpenGL::FGLStateGuard StateGuard{};
-
-                        if (DrawCommand->UserCallback == nullptr) return;
-
-                        const auto& [SlabWindow, PlatformWindow] = *static_cast<FCallbackData*>(DrawCommand->UserCallbackData);
-
-                        glPushAttrib(GL_VIEWPORT_BIT);
-                        SlabWindow->ImmediateDraw(*PlatformWindow);
-                        glPopAttrib();
-                    };
-
-                    ImGui::GetWindowDrawList()->AddCallback(Callback, &CallBackData);
-                }
+                ImGui::GetWindowDrawList()->AddCallback(Callback, &CallBackData);
             }
 
             ImGui::End();
