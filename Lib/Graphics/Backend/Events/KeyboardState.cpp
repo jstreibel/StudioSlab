db950d18df36141a6c81847933cc0d1a9ebbc699
joao <jstreibel@gmail.com>
2025-10-26 23:18:09 -0300

    Added missing files

---
+++ Lib/Graphics/Backend/Events/KeyboardState.cpp
@@ -0,0 +1,52 @@
+//
+// Created by joao on 11/11/24.
+//
+
+#include "KeyboardState.h"
+
+namespace Slab::Graphics {
+
+    const FKeyboardState::FKeyRecord FKeyboardState::DefaultKeyRecord{};
+
+    bool FKeyboardState::NotifyKeyboard(EKeyMap key, EKeyState state, EModKeys modKeys) {
+        ModKeys = modKeys;
+        LastKey = key;
+        SinceLastKeyEvent.reset();
+
+        auto &record = KeyStates[key];
+
+        if (record.State != state) {
+            record.State = state;
+            record.SinceStateChange.reset();
+        } else if (state == Repeat) {
+            record.SinceStateChange.reset();
+        }
+
+        return false;
+    }
+
+    bool FKeyboardState::NotifyCharacter(UInt codepoint) {
+        LastCodepoint = codepoint;
+        SinceLastCharacterEvent.reset();
+        return false;
+    }
+
+    bool FKeyboardState::IsPressed(EKeyMap key) const {
+        return GetState(key) != Release;
+    }
+
+    EKeyState FKeyboardState::GetState(EKeyMap key) const {
+        if (const auto it = KeyStates.find(key); it != KeyStates.end()) {
+            return it->second.State;
+        }
+        return Release;
+    }
+
+    const FKeyboardState::FKeyRecord& FKeyboardState::GetKeyRecord(EKeyMap key) const {
+        if (const auto it = KeyStates.find(key); it != KeyStates.end()) {
+            return it->second;
+        }
+        return DefaultKeyRecord;
+    }
+
+}
---
+++ Lib/Graphics/Backend/Events/KeyboardState.h
@@ -0,0 +1,50 @@
+//
+// Created by joao on 11/11/24.
+//
+
+#ifndef STUDIOSLAB_KEYBOARDSTATE_H
+#define STUDIOSLAB_KEYBOARDSTATE_H
+
+#include "KeyMap.h"
+#include "SystemWindowEventListener.h"
+
+#include "Utils/Map.h"
+#include "Utils/Timer.h"
+
+namespace Slab::Graphics {
+
+    class FKeyboardState final : public FPlatformWindowEventListener {
+    public:
+        struct FKeyRecord {
+            EKeyState State = Release;
+            FTimer SinceStateChange;
+        };
+
+        FKeyboardState() = default;
+        ~FKeyboardState() override = default;
+
+        bool NotifyKeyboard(EKeyMap key, EKeyState state, EModKeys modKeys) override;
+        bool NotifyCharacter(UInt codepoint) override;
+
+        [[nodiscard]] bool IsPressed(EKeyMap key) const;
+        [[nodiscard]] EKeyState GetState(EKeyMap key) const;
+        [[nodiscard]] const FKeyRecord& GetKeyRecord(EKeyMap key) const;
+
+        EModKeys ModKeys{};
+
+        EKeyMap LastKey = Key_UNKNOWN;
+        FTimer SinceLastKeyEvent;
+
+        UInt LastCodepoint = 0;
+        FTimer SinceLastCharacterEvent;
+
+    private:
+        using KeyMapStorage = HashMap<EKeyMap, FKeyRecord>;
+        KeyMapStorage KeyStates{};
+
+        static const FKeyRecord DefaultKeyRecord;
+    };
+
+}
+
+#endif //STUDIOSLAB_KEYBOARDSTATE_H
