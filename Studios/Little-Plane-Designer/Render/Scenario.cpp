2cf3d14a85373ffe6061d1b7bfea9e910df983e9
joao <jstreibel@gmail.com>
2025-11-30 21:35:22 -0300

    Codex added parallax mountains [missing files]

---
+++ Studios/Little-Plane-Designer/Render/Scenario.cpp
@@ -0,0 +1,94 @@
+//
+// Created by joao on 12/18/25.
+//
+
+#include "Scenario.h"
+
+#include <algorithm>
+#include <cmath>
+
+#include "Graphics/OpenGL/LegacyGL/LegacyMode.h"
+#include "Graphics/OpenGL/LegacyGL/SceneSetup.h"
+
+using Slab::Graphics::FColor;
+
+const std::array<FScenario::FMountainLayer, 3> FScenario::Layers = {{
+    {
+        .Parallax = 0.18f,
+        // .BaseHeight = 14.5f,
+        .BaseHeight = 2.5f,
+        .Amplitude = 5.5f,
+        .Frequency = 0.085f,
+        .Phase = 0.0f,
+        .Depth = -0.95f,
+        .Color = FColor::FromHex("#0d2f4c")},
+    {
+        .Parallax = 0.35f,
+        // .BaseHeight = 12.0f,
+        .BaseHeight = 1.0f,
+        .Amplitude = 4.0f,
+        .Frequency = 0.120f,
+        .Phase = 1.3f,
+        .Depth = -0.90f,
+        .Color = FColor::FromHex("#124166")},
+    {
+        .Parallax = 0.55f,
+        // .BaseHeight = 9.0f,
+        .BaseHeight = 0.5f,
+        .Amplitude = 3.0f,
+        .Frequency = 0.185f,
+        .Phase = 2.45f,
+        .Depth = -0.85f,
+        .Color = FColor::FromHex("#195276")
+    },
+}};
+
+float FScenario::ApplyParallax(const float x, const float parallax, const float viewCenter) {
+    return parallax * x + (1.0f - parallax) * viewCenter;
+}
+
+float FScenario::Ridge(const float x, const FMountainLayer& layer) {
+    const float base = std::sin(layer.Frequency * x + layer.Phase);
+    const float slow = 0.55f * std::sin(0.55f * layer.Frequency * x + 0.35f + 0.65f * layer.Phase);
+    const float fast = 0.35f * std::sin(1.9f * layer.Frequency * x + 1.1f * layer.Phase);
+
+    const float crest = std::abs(base + slow + fast);
+    return layer.BaseHeight + layer.Amplitude * crest;
+}
+
+void FScenario::Draw(const Slab::Graphics::FDraw2DParams& Params) {
+    namespace LegacyGL = Slab::Graphics::OpenGL::Legacy;
+
+    LegacyGL::SetupLegacyGL();
+    LegacyGL::PushLegacyMode();
+    LegacyGL::PushScene();
+    LegacyGL::ResetModelView();
+    LegacyGL::SetupOrtho(Params.Region);
+
+    glDisable(GL_TEXTURE_2D);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    const float viewWidth = Params.Region.GetWidth();
+    const float parallaxMargin = 1.f * viewWidth;
+    const float sampleStep = std::max(0.5f, viewWidth / 100.0f);
+    const float xStart = Params.Region.xMin - parallaxMargin;
+    const float xEnd = Params.Region.xMax + parallaxMargin;
+    const float baseY = Params.Region.yMin - 15.0f;
+    const float viewCenter = Params.Region.xCenter();
+
+    for (const auto& layer : Layers) {
+        glColor4f(layer.Color.r, layer.Color.g, layer.Color.b, layer.Color.a);
+
+        glBegin(GL_TRIANGLE_STRIP);
+        for (float s = xStart; s <= xEnd + sampleStep; s += sampleStep) {
+            const float x = ApplyParallax(s, layer.Parallax, viewCenter);
+            const float y = Ridge(s, layer);
+            glVertex3f(x, baseY, layer.Depth);
+            glVertex3f(x, y, layer.Depth);
+        }
+        glEnd();
+    }
+
+    LegacyGL::PopScene();
+}
---
+++ Studios/Little-Plane-Designer/Render/Scenario.h
@@ -0,0 +1,34 @@
+//
+// Created by joao on 12/18/25.
+//
+
+#ifndef STUDIOSLAB_FSCENARIO_H
+#define STUDIOSLAB_FSCENARIO_H
+
+#include <array>
+
+#include "Graphics/Interfaces/IDrawable.h"
+#include "Graphics/Styles/Colors.h"
+#include "Graphics/Types2D.h"
+
+struct FScenario final : Slab::Graphics::IDrawable2D {
+    void Draw(const Slab::Graphics::FDraw2DParams&) override;
+
+private:
+    struct FMountainLayer {
+        float Parallax = 1.0f;
+        float BaseHeight = 0.0f;
+        float Amplitude = 0.0f;
+        float Frequency = 0.0f;
+        float Phase = 0.0f;
+        float Depth = -0.5f;
+        Slab::Graphics::FColor Color;
+    };
+
+    static float ApplyParallax(float x, float parallax, float viewCenter);
+    static float Ridge(const float x, const FMountainLayer& layer);
+
+    static const std::array<FMountainLayer, 3> Layers;
+};
+
+#endif //STUDIOSLAB_FSCENARIO_H
